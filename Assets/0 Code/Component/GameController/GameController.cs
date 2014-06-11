c7bf551c2c4c2039e49f39bb9a3d499fa4027df5
Xackery Xtal <xackery@gmail.com>
2014-06-11 16:43:28 -0700

    Created GameController to handle Game Init, and BaseGameData.

---
+++ Assets/0 Code/Component/GameController/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : BaseComponent {
+	private IGameEntity gameEntity;
+	private ILogger gameLog;
+
+	void Awake() {
+		BaseComponentBegin();
+		if (gameEntity == null) gameEntity = new RaceXStartup();
+		gameEntity.Initialize();
+		DontDestroyOnLoad(transform);
+	}
+
+	void OnApplicationQuit() {
+		gameEntity.Closing();
+	}
+
+}
---
+++ Assets/0 Code/Data/GameData/BaseGameData.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BaseGameData : BaseData {
+	protected float timePlayingTotal;
+	protected float timePlayingThisSession;
+}
