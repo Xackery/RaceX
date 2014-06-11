626d2d2242c531e3fd97c0ba1b770a81b1261366
Xackery Xtal <xackery@gmail.com>
2014-06-11 16:43:01 -0700

    Created GameEntity interface, and RaceXEntity implementation.

---
+++ Assets/0 Code/Entity/IGameEntity/IGameEntity.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IGameEntity : IEntity {
+	void Initialize();
+	void Begin();
+	void Closing();
+	void Shutdown();
+}
---
+++ Assets/0 Code/Entity/IGameEntity/RaceXEntity/RaceXEntity.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaceXStartup : IGameEntity {
+	private bool isInitialized;
+
+	public void Initialize() {
+		if (isInitialized) return;
+		Debug.Log ("Starting");
+		isInitialized = true;
+	}
+
+	public void Begin() {
+	}
+
+	public void Closing() {
+		Debug.Log ("Closing");
+	}
+
+	public void Shutdown() {
+	}
+}
