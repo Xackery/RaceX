4dc17db5bf476cc3312e85e446664fe012e2cf37
Xackery Xtal <xackery@gmail.com>
2014-06-11 16:42:32 -0700

    Added ILogger Interface with DebugLogger and ScreenLogger implementations

---
+++ Assets/0 Code/Entity/ILogger/ILogger.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+interface ILogger : IEntity {
+	void Log(string message);
+}
---
+++ Assets/0 Code/Entity/ILogger/ScreenLogger/ScreenLogger.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenLogger : ILogger {
+	public void Log(string message) {
+		//Todo: Add Screen Logger
+	}
+}
