b8ed984242bc1c0cff2307ce402958235ffc94ec
Xackery Xtal <xackery@gmail.com>
2014-06-14 00:37:45 -0700

    Preliminary driving stuff

--- Assets/0 Code/Monobehavior/Tank/TankMod/Engine.cs
+++ Assets/0 Code/Monobehavior/Tank/TankMod/Engine.cs
@@ -7,24 +7,27 @@ using System.Collections;
 public class Engine : MonoBehaviour
 {
 	private bool isForward = false;
+	private bool isLeft = false;
 	public float currentForce;
 
 
 	void FixedUpdate () {
+		if (Input.GetButton("Horizontal")) {
+			bool isLeft = (Input.GetAxis ("Horizontal") > 0);
+			transform.Rotate((isLeft) ? new Vector3(0,1,0) : new Vector3(0,-1,0));
+		}
 
-		if (Input.GetButtonDown("Throttle")) {
+		if (Input.GetButton("Vertical")) {
 			isForward = true;
+		} else if (isForward) {
+			isForward = false;
+			currentForce = 0;
 		}
 
 		if (isForward) {
-			float force = Mathf.Clamp(currentForce,1,20000) * 2;
-			rigidbody.AddRelativeForce(transform.forward * force);
-			currentForce = Mathf.Clamp(force, 1, 20000);
-		}
-
-		if (isForward && Input.GetButtonUp("Throttle")) {
-			isForward = false;
-			currentForce = 0;
+			float force = Mathf.Clamp(currentForce,1,10000) * 1.2f;
+			rigidbody.AddForce(transform.forward * force);
+			currentForce = Mathf.Clamp(force, 1, 10000);
 		}
 
 	}
